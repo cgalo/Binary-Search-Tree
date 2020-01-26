81877aa48c62f50d87e066fd7d94b57085a03c9e
cgalo <cgalo13@gmail.com>
2020-01-26 12:51:21 -0500

    Updating BSTree object and started to create a menu on the main class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
 
-void menu (std::string cmd, std::string word);  //Declaring the menu method
-void toUpper (std::string* word);               //Declaring the toUpper method, makes any word into uppercase
+void menu           (std::string cmd, std::string word);    //Declaring the menu function
+void toUpper        (std::string* word);                    //Declaring the toUpper function
+std::string getWord (std::string line, int wordIndex);      //Declaring getWord function
 
 int main()
 {
@@ -13,8 +14,13 @@ int main()
         std::cin >> input;
         //Now we need to split the input to get the cmd and the word we want to store
 
+        std::string cmd = getWord(input, 1);
+        std::string word = getWord(input, 2);
 
-    } while (input == "EXIT");
+        std::cout << "cmd: " << cmd << std::endl;
+        std::cout << "word: " << word << std::endl;
+
+    } while (input != "EXIT");
 
     return 0;
 }
@@ -22,10 +28,33 @@ int main()
 void menu(std::string cmd, std::string word)
 {
 
-}
+}   //End of menu function
 
 //Function takes pointer/reference of a string and converts it into uppercase
 void toUpper (std::string* word)
 {
     for (auto & c: *word) c = toupper(c);
 }   //End of toUpper function
+
+std::string getWord (std::string line, int wordIndex)
+{
+    int countWord = 0;      //Total read words
+    std::string returnWord; //To store the word to return
+    for (int i = 0 ; i < line.length(); i++) { // iterate over all characters in 'line'
+        if (line[i] == ' ') { // if this character is a space we might be done reading a word from 'line'
+            if (line[i+1] != ' ') { // next character is not a space, so we are done reading a word
+                countWord++; // increase number of read words
+                if (countWord == wordIndex) { // was this the word we were looking for?
+                    break; // yes it was, so return it
+                }
+                returnWord =""; // nope it wasn't .. so reset word and start over with the next one in 'line'
+            }
+        }
+        else { // not a space .. so append the character to 'word'
+            returnWord += line[i];
+        }
+    }
+    return returnWord;
+}   //End of getWord function
+
+
